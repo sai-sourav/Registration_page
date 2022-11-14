8267651047f22b4f1c9ad7e75e06a726eb7f64d4
sai-sourav <saisourav123@gmail.com>
2022-11-14 05:36:19 +0530

    crud crud update post

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const myform = document.querySelector("#my-form");
-const name = document.querySelector("#name");
-const email = document.querySelector("#email");
-const phone = document.querySelector("#phone");
-const calldate = document.querySelector("#calldate");
-const calltime = document.querySelector("#calltime");
 myform.addEventListener('submit', onsubmit);
 
 function onsubmit(event){
     event.preventDefault();
-    if (name.value !== '' || email.value !== '' ||
-        phone.value !== '' || calldate.value !== '' ||
-        calltime.value !== ''){
-            console.log(name.value)
-    }
+    // if (name.value !== '' || email.value !== '' ||
+    //     phone.value !== '' || calldate.value !== '' ||
+    //     calltime.value !== ''){
+    //         console.log(name.value)
+    // }
+    // save on crud crud server change it every 24 hrs
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const phone = document.querySelector("#phone").value;
+    const calldate = document.querySelector("#calldate").value;
+    const calltime = document.querySelector("#calltime").value;
+    axios.post('https://crudcrud.com/api/5280ca81cac042c9af8990236e33b0b5/appointmentData',{
+        "name": name,
+        "emailid": email,
+        "phone" : phone,
+        "calldate" : calldate,
+        "calltime" : calltime
+      })
+        .then (res => console.log(res))
+        .catch (err => console.log(err));
     
 }

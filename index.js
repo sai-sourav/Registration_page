08e6ec59be4c23a8612e6af8be83aad0afa608bf
sai-sourav <saisourav123@gmail.com>
2022-11-14 11:17:09 +0530

    added get method of crudcrud

--- index.js
+++ index.js
@@ -6,23 +6,18 @@ itemList.addEventListener('click', removeItem);
 itemList.addEventListener('click', editItem);
 // on submit
 myform.addEventListener('submit', onsubmit);
-
-showlist();
+// on reload
+window.addEventListener('DOMContentLoaded', showlist);
 
 function onsubmit(event){
     event.preventDefault();
-    // if (name.value !== '' || email.value !== '' ||
-    //     phone.value !== '' || calldate.value !== '' ||
-    //     calltime.value !== ''){
-    //         console.log(name.value)
-    // }
     // save on crud crud server change it every 24 hrs
     const name = document.querySelector("#name").value;
     const email = document.querySelector("#email").value;
     const phone = document.querySelector("#phone").value;
     const calldate = document.querySelector("#calldate").value;
     const calltime = document.querySelector("#calltime").value;
-    axios.post('https://crudcrud.com/api/5280ca81cac042c9af8990236e33b0b5/appointmentData',{
+    axios.post('https://crudcrud.com/api/392ddf094f374650ba84ef186aa86dbc/appointmentData/',{
         "name": name,
         "emailid": email,
         "phone" : phone,
@@ -32,71 +27,70 @@ function onsubmit(event){
         .then (res => console.log(res))
         .catch (err => console.log(err));
     // save them on local storage
-    let myobj = {
-        "name": name,
-        "emailid": email,
-        "phone" : phone,
-        "calldate" : calldate,
-        "calltime" : calltime
-    }
-    let myobj_serialized = JSON.stringify(myobj);
-    localStorage.setItem("user"+myobj.name,myobj_serialized);
     showlist();
-    document.getElementById('name').value = " ";
-    document.getElementById('email').value = " ";
-    document.querySelector("#phone").value = " ";
-    document.querySelector("#calldate").value = " ";
-    document.querySelector("#calltime").value = " ";
 
+}
+
+function refreshlocalstorage(obj) {
+    let string_obj = JSON.stringify(obj);
+    localStorage.setItem(obj._id,string_obj);
 }
 
 
 function showlist(){
-    itemList.innerHTML = ""
-    Object.keys(localStorage).forEach((key)=>{
-        var user_serialized = localStorage.getItem(key);
-        var user_deserialized = JSON.parse(user_serialized);
-        appendlist(user_deserialized.name + " " + user_deserialized.emailid);
-    });
+    itemList.innerHTML = "";
+    localStorage.clear();
+    axios.get('https://crudcrud.com/api/392ddf094f374650ba84ef186aa86dbc/appointmentData')
+    .then(response => {
+        for(let i = 0; i< response.data.length; i++){
+            appendlist(response.data[i]);
+            refreshlocalstorage(response.data[i]);
+        }
+    })
+
 }
 
-function appendlist(text){
-    // Create new li element
+function appendlist(obj){
+  let text = obj.name + " " + obj.emailid;
+//   let newelement = `<li class="list-group-item" id="${obj._id}"> ${text} 
+//                         <button class="btn btn-danger btn-sm float-right delete">delete</button>
+//                         <button class="btn btn-default btn-sm float-right edit">edit</button> 
+//                     </li>`
   var li = document.createElement('li');
-  // Add class
-  li.className = 'list-group-item';
-  // Add text node with input value
-  li.appendChild(document.createTextNode(text));
-  ///
+//   li.innerHTML = newelement;
+li.className = 'list-group-item';
 
-  // Create del button element
-  var deleteBtn = document.createElement('button');
+li.id = obj._id;
+// Add text node with input value
+li.appendChild(document.createTextNode(text));
+///
 
-  // Add classes to del button
-  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+// Create del button element
+var deleteBtn = document.createElement('button');
 
-  // Append text node
-  deleteBtn.appendChild(document.createTextNode('X'));
+// Add classes to del button
+deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
 
-  // Append button to li
-  li.appendChild(deleteBtn);
-  
-  ///
+// Append text node
+deleteBtn.appendChild(document.createTextNode('X'));
 
-  // create edit button element
-  var editBtn = document.createElement('button');
+// Append button to li
+li.appendChild(deleteBtn);
 
-  // Add classes to edit button
-  editBtn.className = 'btn btn-default btn-sm float-right edit';
+///
 
-   // Append text node
-   editBtn.appendChild(document.createTextNode('Edit'));
+// create edit button element
+var editBtn = document.createElement('button');
 
-   // Append button to li
+// Add classes to edit button
+editBtn.className = 'btn btn-default btn-sm float-right edit';
+
+ // Append text node
+ editBtn.appendChild(document.createTextNode('Edit'));
+
+ // Append button to li
   li.appendChild(editBtn);
 
-
-  ///
 
   // Append li to list
   itemList.appendChild(li);
@@ -107,14 +101,14 @@ function appendlist(text){
 function removeItem(e){
     if(e.target.classList.contains('delete')){
       if(confirm('Are You Sure?')){
+        const itemList = document.getElementById("items");
         var li = e.target.parentElement;
-        let arr = li.innerText
-        arr = arr.replace("XEdit",'');
-        arr = arr.split(' ')
-        arr.pop();
-        arr = arr.join(' ')
+        let obj_string = localStorage.getItem(li.id);
+        let obj = JSON.parse(obj_string);
+        let URL = `https://crudcrud.com/api/392ddf094f374650ba84ef186aa86dbc/appointmentData/${obj._id}`
+        axios.delete(URL).then(res => console.log(res)).catch(err => console.log(err));
+        localStorage.removeItem(li.id);
         itemList.removeChild(li);
-        localStorage.removeItem("user"+arr)
       }
     }
   }
@@ -123,16 +117,14 @@ function removeItem(e){
   function editItem(e){
     if(e.target.classList.contains('edit')){
         var li = e.target.parentElement;
-        let arr = li.innerText
-        console.log(arr)
-        arr = arr.replace("XEdit",'');
-        arr = arr.split(' ')
-        let email = arr.pop();
-        let fullname = arr.join(' ')
         // delete element
-        itemList.removeChild(li);
-        let obj = localStorage.getItem("user"+fullname)
-        localStorage.removeItem("user"+fullname)
+        let obj_string = localStorage.getItem(li.id);
+        let obj1 = JSON.parse(obj_string);
+        let URL = `https://crudcrud.com/api/392ddf094f374650ba84ef186aa86dbc/appointmentData/${obj1._id}`
+        axios.delete(URL).then(res => console.log(res)).catch(err => console.log(err));
+        let obj = localStorage.getItem(li.id);
+        localStorage.removeItem(li.id);
+        itemList.remove(li);
         document.getElementById('name').value = obj.name;
         document.getElementById('email').value = obj.email;
         document.querySelector("#phone").value = obj.phone;

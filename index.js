7790e6fc08dfd725042002218b7477bece504d47
sai-sourav <saisourav123@gmail.com>
2022-11-14 06:46:15 +0530

    with internal storage and crudcrud post

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const myform = document.querySelector("#my-form");
+const itemList = document.getElementById("items");
+// Delete event
+itemList.addEventListener('click', removeItem);
+// edit event
+itemList.addEventListener('click', editItem);
+// on submit
 myform.addEventListener('submit', onsubmit);
+
+showlist();
 
 function onsubmit(event){
     event.preventDefault();
@@ -23,5 +31,104 @@ function onsubmit(event){
       })
         .then (res => console.log(res))
         .catch (err => console.log(err));
-    
+    // save them on local storage
+    let myobj = {
+        "name": name,
+        "emailid": email,
+        "phone" : phone,
+        "calldate" : calldate,
+        "calltime" : calltime
+    }
+    let myobj_serialized = JSON.stringify(myobj);
+    localStorage.setItem("user"+myobj.name,myobj_serialized);
+    showlist();
 }
+
+
+function showlist(){
+    itemList.innerHTML = ""
+    Object.keys(localStorage).forEach((key)=>{
+        var user_serialized = localStorage.getItem(key);
+        var user_deserialized = JSON.parse(user_serialized);
+        appendlist(user_deserialized.name + " " + user_deserialized.emailid);
+    });
+}
+
+function appendlist(text){
+    // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(text));
+  ///
+
+  // Create del button element
+  var deleteBtn = document.createElement('button');
+
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  // Append button to li
+  li.appendChild(deleteBtn);
+  
+  ///
+
+  // create edit button element
+  var editBtn = document.createElement('button');
+
+  // Add classes to edit button
+  editBtn.className = 'btn btn-default btn-sm float-right edit';
+
+   // Append text node
+   editBtn.appendChild(document.createTextNode('Edit'));
+
+   // Append button to li
+  li.appendChild(editBtn);
+
+
+  ///
+
+  // Append li to list
+  itemList.appendChild(li);
+}
+
+
+// Remove item
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+      if(confirm('Are You Sure?')){
+        var li = e.target.parentElement;
+        let arr = li.innerText
+        arr = arr.replace("XEdit",'');
+        arr = arr.split(' ')
+        arr.pop();
+        arr = arr.join(' ')
+        itemList.removeChild(li);
+        localStorage.removeItem("user"+arr)
+      }
+    }
+  }
+  
+  // Edit item
+  function editItem(e){
+    if(e.target.classList.contains('edit')){
+        var li = e.target.parentElement;
+        let arr = li.innerText
+        console.log(arr)
+        arr = arr.replace("XEdit",'');
+        arr = arr.split(' ')
+        let email = arr.pop();
+        let fullname = arr.join(' ')
+        // delete element
+        itemList.removeChild(li);
+        localStorage.removeItem("user"+fullname)
+        document.getElementById('name').value = fullname;
+        document.getElementById('email').value = email;
+    }
+  }
+
+
